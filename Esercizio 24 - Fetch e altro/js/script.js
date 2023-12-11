83e5a2d08096e846352fcdbef58d79f049b8b2c0
xhoana2322 <xhoana.beqiri23@gmail.com>
2023-12-11 18:20:18 +0100

    esercizio 24 - fetch e altro

--- Esercizio 24 - Fetch e altro/js/script.js
+++ Esercizio 24 - Fetch e altro/js/script.js
@@ -1,46 +1,40 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     document.querySelector('#load-images').addEventListener('click', () => {
-//         fetch('https://api.pexels.com/v1/search?query=[your-query]', {
-//             method: 'GET',
-//             headers: {
-//                 Authorization: 'VWQ5WbtmtniFsJeXlEjdPCcMeO6U6MaW8Uf4xAIshjHYoALhzvANQg9B'
-//             }
-//         }).then(response => response.json())
-//         .then(json => {
-    
-//         })
-//         .catch(error => console.log(error))
-//     })
-// })
 
-
+// button 1
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#load-images').addEventListener('click', () => {
-        fetch('https://api.pexels.com/v1/search?query=[your-query]', {
+        fetch('https://api.pexels.com/v1/search?query=cat', {
             method: 'GET',
             headers: {
                 Authorization: 'VWQ5WbtmtniFsJeXlEjdPCcMeO6U6MaW8Uf4xAIshjHYoALhzvANQg9B'
             }
         })
             .then(response => response.json())
-            .then(json => console.log(json))
             .then(json => {
-                // json.photos.forEach(photo => {
-                //     let img = document.querySelector('img');
-                //     img.classList.add('src');
-                //     img.src = json.photos[0].src.small;
-                //     console.log(json.photos[0].src.small)
-                // });
-
-                let img = document.querySelectorAll('.card img');
-                console.log(json.photos)
-                console.log(json.photos[0].src.medium);
-                    
-                img.forEach(element => {
-                    img.src = json.element[0].src.small;
+                let img = document.querySelectorAll('.card img')
+                    img.forEach((img, i) => {
+                        img.src = json.photos[i].src.medium;
+                    })
                 })
+            .catch(error => console.log(error))
+    })
+})
 
-            })
+// button 2
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#load-images-secondary').addEventListener('click', () => {
+        fetch('https://api.pexels.com/v1/search?query=dog', {
+            method: 'GET',
+            headers: {
+                Authorization: 'VWQ5WbtmtniFsJeXlEjdPCcMeO6U6MaW8Uf4xAIshjHYoALhzvANQg9B'
+            }
+        })
+            .then(response => response.json())
+            .then(json => {
+                let img = document.querySelectorAll('.card img')
+                    img.forEach((img, i) => {
+                        img.src = json.photos[i].src.medium;
+                    })
+                })
             .catch(error => console.log(error))
     })
 })
